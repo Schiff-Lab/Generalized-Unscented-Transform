9bbcfb3517b47de3ee6455ebf77596dc0d86a147
debeigbe <93268832+debeigbe@users.noreply.github.com>
2022-06-10 10:58:54 -0400

    Update Case_study_3
    Modified example to include a correlated Poisson random vector

--- Case_study_3.m
+++ Case_study_3.m
@@ -6,6 +6,19 @@ addpath('Distribution Moments');
 addpath('Unscented Transforms');
 
 %%
+% Generate correlated Poisson random numbers using sum of independent
+% Poisson random numbers
+mu3 = 0.2; % Third mean
+mu1 = 9.8; mu2 = 1.8;  % First and Second Mean
+
+mu_x1 = mu1 + mu3;  % Mean of I
+mu_x2 = mu2 + mu3;  % Mean of R
+P_x1_x2 = [mu_x1   mu3;  mu3  mu_x2]; % Covariance showing correlation
+
+rho = mu3/ sqrt( (mu1 + mu3)*(mu2 + mu3)  ); % Pearson Correlation coefficient
+
+
+%%
 I = 10; R = 2; beta = 1.5; N = 100; gamma = 0.3;
 
 % Analytic raw moments of Poissin(I) 
@@ -18,27 +31,31 @@ E_R_2 = R^2 + R;   % E[R^2]
 E_R_3 = R^3 + 3*R^2 + R; % E[I^3]
 E_R_4 = R^4 + 6*R^3 + 7*R^2 + R; % E[I^4]
 
+% Mean of x1*x2. That is E(X1*X2)
+E_x1_x2 = mu_x1*mu_x2  + mu3;   % E(X1*X2)
+
 % Analytic mean of reduced SIR model 
-y_mean_true = [I; R] + [beta*(N*I - E_I_2 - I*R)/N ;  gamma*I ];
-% Analytic covariance of reduced SIR model 
-cov_true_11 = I^2*beta^2 + E_I_2*beta^2 - (2*I^2*beta^2)/N + (I^2*beta^2)/N^2 - (2*I^3*beta^2)/N + (2*I^3*beta^2)/N^2 + (I^4*beta^2)/N^2 - (2*E_I_3*beta^2)/N + (E_I_4*beta^2)/N^2 - 2*I*I*beta^2 + (I^2*R^2*beta^2)/N^2 - (2*I^2*E_I_2*beta^2)/N^2 + (E_I_2*E_R_2*beta^2)/N^2 + (2*I*I*beta^2)/N - (2*I^2*R*beta^2)/N + (2*I^2*R*beta^2)/N^2 + (2*I^3*R*beta^2)/N^2 + (2*I*E_I_2*beta^2)/N + (2*I^2*I*beta^2)/N - (2*I*E_I_2*beta^2)/N^2 - (2*E_I_2*R*beta^2)/N + (2*E_I_3*R*beta^2)/N^2 - (2*I*R*E_I_2*beta^2)/N^2 - (2*I^2*I*R*beta^2)/N^2 + (2*I*R*I*beta^2)/N + (2*I*I*R*beta^2)/N - (2*I*I*R*beta^2)/N^2 - (2*I*R*I*R*beta^2)/N^2;
-cov_true_22 = gamma^2*E_I_2 - 2*I*gamma^2*I + I^2*gamma^2;
-cov_true_12 = I^2*beta*gamma + E_I_2*beta*gamma - (I^2*beta*gamma)/N - (I^3*beta*gamma)/N - (E_I_3*beta*gamma)/N - 2*I*I*beta*gamma + (I*I*beta*gamma)/N - (I^2*R*beta*gamma)/N + (I*E_I_2*beta*gamma)/N + (I^2*I*beta*gamma)/N - (E_I_2*R*beta*gamma)/N + (I*R*I*beta*gamma)/N + (I*I*R*beta*gamma)/N;
-cov_true_21 = cov_true_12;
-y_cov_true = [cov_true_11  cov_true_12;  cov_true_21  cov_true_22]; 
+y_mean_true = [I; R] + [beta*(N*I - E_I_2 - E_x1_x2)/N ;  gamma*I ];
 
 %% Evaluate accuracy in propagating means and covariances
 n = 2;                  % State dimension
 mu = [I; R];            % Mean
-P = diag(mu);           % Covariance
+P = P_x1_x2;           % Covariance
 skewX = mu;             % Diagonal component of skewness
 kurtX = 3*mu.^2 + mu;   % Diagonal component of kurtosis
 
 % Monte Carlo Draws
-x = poissrnd(mu.*ones(2, 100000));
+nummonte = 10000000;
+% Generate correlated Poisson random numbers using sum of independent
+% Poisson random numbers
+Y1 = poissrnd(mu1.*ones(1, nummonte)); Y2 = poissrnd(mu2.*ones(1, nummonte));  Y3 = poissrnd(mu3.*ones(1, nummonte));
+X1 = Y1 + Y3;   X2 = Y2 + Y3; % New Poisson random numbers
+x = [X1; X2];
 y_monte100000 = nonlinTrans(x);
 y_monte_mean100000 = mean(y_monte100000,2);
+% covariance of reduced SIR model using 10^7 Monte Carlo draws
 y_monte_cov100000 = cov(y_monte100000');
+y_cov_true = y_monte_cov100000; % Use Monte Carlo as true covariance
 
 % Standard UT Draws
 [sigma_UT, weights_UT] = unscentedEnsemble(mu, P, sqrt(3));
@@ -79,6 +96,3 @@ function y = nonlinTrans(x)
 I = 10; R = 2; beta = 1.5; N = 100; gamma = 0.3;
 y = [I; R] + [beta*(N-x(1,:) - x(2,:)).*x(1,:)/N ;  gamma*x(1,:) ];
 end
-
-
- 
